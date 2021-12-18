79609659867798fe84b686af70cb7ff74331f8f3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-18 09:47:25 -0300

    init

---
+++ lib/router.dart
@@ -0,0 +1,20 @@
+import 'package:finances/pages/home.dart';
+import "package:flutter/material.dart";
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (_) => HomePage());
+      default:
+        // TODO
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text("No route defined for ${settings.name}"),
+            ),
+          ),
+        );
+    }
+  }
+}
