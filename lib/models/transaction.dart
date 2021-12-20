1cca4bdcb69b174aa042a00210f1471e6928f4b2
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-20 17:46:31 -0300

    added TransactionType enum

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,7 +1,13 @@
+enum TransactionType {
+  Income,
+  Expense,
+}
+
 class Transaction {
   final int id;
   final int amount;
   final String date;
+  final TransactionType type;
 
-  Transaction(this.id, this.amount, this.date);
+  Transaction(this.id, this.amount, this.date, this.type);
 }
