9306d7480028c11f0b73384f556bbbd4d72d21fb
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-22 14:36:13 -0300

    changed display of stats, added formattedCurrency fn

--- lib/pages/home/quick_stats.dart
+++ lib/pages/home/quick_stats.dart
@@ -2,75 +2,134 @@ import 'package:finances/providers/transactions_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+const thousand = 1000;
+const million = 1000000;
+const billion = 1000000000;
+const trillion = 1000000000000;
+
+String formatCurrency(double value) {
+  print(value);
+  if (value == 0) {
+    return "0";
+  } else if (value > trillion) {
+    return '${(value / trillion).toStringAsFixed(1)}T';
+  } else if (value > billion) {
+    return '${(value / billion).toStringAsFixed(1)}B';
+  } else if (value > million) {
+    return '${(value / million).toStringAsFixed(1)}M';
+  } else if (value > thousand) {
+    return '${(value / thousand).toStringAsFixed(1)}K';
+  } else {
+    return value.toStringAsFixed(2);
+  }
+}
+
 class QuickStats extends StatelessWidget {
   const QuickStats({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var transactions = Provider.of<TransactionsProvider>(context);
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.horizontal(
-                left: Radius.circular(7),
+    return Container(
+      height: 110,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.horizontal(
+                  left: Radius.circular(7),
+                ),
+                color: Colors.green[500],
               ),
-              color: Colors.green[500],
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Income",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      formatCurrency(transactions.totalIncome),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ]),
             ),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Text(
-                "Income",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "\$" + transactions.totalIcome.toString(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-            ]),
           ),
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.horizontal(
-                right: Radius.circular(7),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.red[500],
               ),
-              color: Colors.red[500],
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Expenses",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      formatCurrency(transactions.totalExpenses),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ]),
             ),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Text(
-                "Expenses",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "\$" + transactions.totalExpenses.toString(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-            ]),
           ),
-        ),
-      ],
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.horizontal(
+                  right: Radius.circular(7),
+                ),
+                color: Colors.orange[700],
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Total",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      (transactions.total >= 0 ? "" : "-") +
+                          formatCurrency(transactions.total.abs()),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
