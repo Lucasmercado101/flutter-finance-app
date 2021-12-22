dc0aba66aa31aa2922d6049ef29a3024209e81e4
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-22 14:55:04 -0300

    moved and renamed formatCurrency to utils.dart

--- lib/pages/home/quick_stats.dart
+++ lib/pages/home/quick_stats.dart
@@ -1,31 +1,7 @@
 import 'package:finances/providers/transactions_provider.dart';
+import 'package:finances/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-const thousand = 1000;
-const million = 1000000;
-const billion = 1000000000;
-const trillion = 1000000000000;
-const quadrillion = 1000000000000000;
-const quintillion = 1000000000000000000;
-
-String formatCurrency(int value) {
-  if (value >= quintillion) {
-    return '${(value / quintillion).toStringAsFixed(2)}Q';
-  } else if (value >= quadrillion) {
-    return '${(value / quadrillion).toStringAsFixed(2)}q';
-  } else if (value >= trillion) {
-    return '${(value / trillion).toStringAsFixed(0)}t';
-  } else if (value >= billion) {
-    return '${(value / billion).toStringAsFixed(0)}B';
-  } else if (value >= million) {
-    return '${(value / million).toStringAsFixed(0)}M';
-  } else if (value >= thousand) {
-    return '${(value / thousand).toStringAsFixed(0)}K';
-  } else {
-    return value.toString();
-  }
-}
 
 class QuickStats extends StatelessWidget {
   const QuickStats({Key? key}) : super(key: key);
@@ -60,7 +36,7 @@ class QuickStats extends StatelessWidget {
                     ),
                     const SizedBox(height: 10),
                     Text(
-                      formatCurrency(transactions.totalIncome),
+                      abrvAmount(transactions.totalIncome),
                       style: const TextStyle(
                         color: Colors.white,
                         fontSize: 22,
@@ -89,7 +65,7 @@ class QuickStats extends StatelessWidget {
                     ),
                     const SizedBox(height: 10),
                     Text(
-                      formatCurrency(transactions.totalExpenses),
+                      abrvAmount(transactions.totalExpenses),
                       style: const TextStyle(
                         color: Colors.white,
                         fontSize: 22,
@@ -122,7 +98,7 @@ class QuickStats extends StatelessWidget {
                     const SizedBox(height: 10),
                     Text(
                       (transactions.total >= 0 ? "" : "-") +
-                          formatCurrency(transactions.total.abs()),
+                          abrvAmount(transactions.total.abs()),
                       style: const TextStyle(
                         color: Colors.white,
                         fontSize: 22,
---
+++ lib/utils.dart
@@ -0,0 +1,24 @@
+const thousand = 1000;
+const million = 1000000;
+const billion = 1000000000;
+const trillion = 1000000000000;
+const quadrillion = 1000000000000000;
+const quintillion = 1000000000000000000;
+
+String abrvAmount(int value) {
+  if (value >= quintillion) {
+    return '${(value / quintillion).toStringAsFixed(2)}Q';
+  } else if (value >= quadrillion) {
+    return '${(value / quadrillion).toStringAsFixed(2)}q';
+  } else if (value >= trillion) {
+    return '${(value / trillion).toStringAsFixed(0)}t';
+  } else if (value >= billion) {
+    return '${(value / billion).toStringAsFixed(0)}B';
+  } else if (value >= million) {
+    return '${(value / million).toStringAsFixed(0)}M';
+  } else if (value >= thousand) {
+    return '${(value / thousand).toStringAsFixed(0)}K';
+  } else {
+    return value.toString();
+  }
+}
